ae3c935cd9d05e50ed858315a3b47a7ac2e1d295
3A <524019351@qq.com>
2019-07-25 23:29:28 +0800

    价值网络

---
+++ test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import keras.backend as K
+
+m1=np.random.randint(0,2,(1))
+m2=np.random.randint(0,100,(2*(5+12)*12))
+
+print(m1)
+print(m2)
+
+a = K.variable(m1)
+b = K.variable(m2)
+c1 = K.concatenate([a,b])
+
+print(K.eval(c1))
---
+++ value_net.py
@@ -0,0 +1,84 @@
+from keras.engine.topology import Input
+from keras.engine.training import Model
+from keras.layers.convolutional import Conv2D, Conv1D
+from keras.layers.core import Activation, Dense, Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+from keras.optimizers import Adam
+import keras.backend as K
+import pickle
+
+
+class PolicyValueNet():
+    def __init__(self, model_file=None):
+        self.board_width = 5
+        self.board_height = 12
+
+        self.l2_const = 1e-4  # coef of l2 penalty
+        self.create_net()
+
+        self.train_op()
+
+        if model_file:
+            net_params = pickle.load(open(model_file, 'rb'))
+            self.model.set_weights(net_params)
+
+
+    def create_net(self):
+        board = Input((self.board_width, self.board_height))
+        probMap = Input((12,12))
+        isFirstHand = Input((1))
+
+        # conv layers
+        network1 = Conv1D(filters=8, kernel_size=3, padding="same", data_format="channels_first",
+                         activation="relu", kernel_regularizer=l2(self.l2_const))(board)
+        network1 = Conv1D(filters=32, kernel_size=3, padding="same", data_format="channels_first",
+                         activation="relu", kernel_regularizer=l2(self.l2_const))(network1)
+        network2 = Conv1D(filters=8, kernel_size=3, padding="same", data_format="channels_first",
+                          activation="relu", kernel_regularizer=l2(self.l2_const))(probMap)
+        network2 = Conv1D(filters=32, kernel_size=3, padding="same", data_format="channels_first",
+                          activation="relu", kernel_regularizer=l2(self.l2_const))(network2)
+
+        network = K.concatenate([network1,network2], axis=1)
+
+        # state value layers
+        value_net = Conv1D(filters=4, kernel_size=1, data_format="channels_first", activation="relu",
+                           kernel_regularizer=l2(self.l2_const))(network)
+        value_net = Conv1D(filters=2, kernel_size=1, data_format="channels_first", activation="relu",
+                           kernel_regularizer=l2(self.l2_const))(value_net)
+        value_net = Flatten()(value_net)
+        value_net = K.concatenate([value_net,isFirstHand]) # 考虑先后手
+        value_net = Dense(32, kernel_regularizer=l2(self.l2_const))(value_net)
+        value_net = Dense(32, kernel_regularizer=l2(self.l2_const))(value_net)
+        self.value_net = Dense(1, activation="tanh", kernel_regularizer=l2(self.l2_const))(value_net)
+
+        self.model = Model([board,probMap,isFirstHand], self.value_net)
+
+
+    def train_op(self):
+        """
+        Three loss terms：
+        loss = (z - v)^2 + pi^T * log(p) + c||theta||^2
+        """
+
+        self.model.compile(optimizer=Adam(), loss='mean_squared_error')
+
+        def train_step(board, probMap, isFirstHand, isWin, learning_rate): # isWin与isFirstHand一样为bool列表，表示是否胜利
+            loss = self.model.evaluate([board,probMap,isFirstHand], isWin, batch_size=len(board),
+                                       verbose=0)
+            K.set_value(self.model.optimizer.lr, learning_rate)
+            # fix:目前所有胜利的局面值都为1，实际应当根据Q值更新公式给予远距离的局面一些折扣？
+            self.model.fit([board,probMap,isFirstHand], isWin, batch_size=len(board))
+            return loss[0]
+
+        self.train_step = train_step
+        return train_step
+
+
+    def get_param(self):
+        net_params = self.model.get_weights()
+        return net_params
+
+
+    def save_model(self, model_file):
+        pickle.dump(self.model, open(model_file, 'wb'), protocol=2)
