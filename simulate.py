4ca6b7f6e1c3e3a26992616a8d649d158f9c0927
GPLme <524019351@qq.com>
2019-07-29 20:10:29 +0800

    简化一下

--- simulate.py
+++ simulate.py
@@ -18,10 +18,8 @@ def simMove(node,x1,y1,x2,y2,isEne):
             node.posList[enemyChess] = (x2,y2)
         cMap[y2][x2]=cMap[y1][x1]
         cMap[y1][x1]=0
-        Ismove=True
-        return cMap,Ismove,posList
+        return cMap,True,posList
     else:
-        Ismove=False
         if isEne==False:
             result=asses.ChessComparisons(cMap[y1][x1],findChess(x2,y2,node.posList),node)
         else:
@@ -56,4 +54,4 @@ def simMove(node,x1,y1,x2,y2,isEne):
             cMap[y2][x2]=0
             cMap[y1][x1]=0
 
-        return cMap,Ismove,posList
+        return cMap,False,posList
