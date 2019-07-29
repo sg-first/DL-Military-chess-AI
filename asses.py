e4765975c4c0a31fd69eb7824b113d455c79f81a
3A <524019351@qq.com>
2019-07-29 11:39:31 +0800

    改错（还剩simMove else中的Ismove没定义没改）

--- asses.py
+++ asses.py
@@ -1,50 +1,57 @@
-def setDie(sub,posList):
+import basic
+
+def setDie(sub,posList): # 仅该函数对posList产生副作用
     posList[sub] = (-1,-1)
+
 def isDie(sub,posList):
     x,y = posList[sub]
     return x==-1 and y==-1
-def codeToStrength(type):#finish
-    if type==11 : # zhadan=11
-        return 7 #炸弹=师长
-    elif type==10: #dilei=10
-        return 5 #地雷=团长
+
+def codeToStrength(type):
+    if type==basic.zhadan:
+        return basic.shizhang # 炸弹=师长
+    elif type==basic.dilei:
+        return basic.tuanzhang # 地雷=团长
     else :
         return type
-def getChessStrength(chess,node):#棋子死亡作为参数传进来？finish
+
+def getChessStrength(chess,node): # 棋子死亡作为参数传进来？finish
     if isDie(chess,node.posList):
         return 0
-    else :
+    else:
         score = 0
         for i in range(len(node.probTable)):
             weight=codeToStrength(i)
-            score+=weight*node.proTable(i)#?
-def isDetermine(node):#finish
+            score+=weight*node.probTable[i]
+
+def isDetermine(node): # finish
     type=-1
     for i in range(len(node.probTable)):
-        if type==-1 and not(node.proTable ==0) :
-            type=i
-        if not(type==-1 ) and not(node.proTable[i]==0):
+        if type==-1 and not(node.probTable[i]==0) :
+            type = i
+        if not(type==-1) and not(node.probTable[i]==0):
             return -1
     return type
+
 def ChessComparisons(myc,enc,node):#finish
     mytype=codeToType(myc)
     encType=isDetermine(enc)
-    #涉及工兵地雷的特判
-    if(mytype==1):#if mytype==gongbing
+    # 涉及工兵地雷的特判
+    if(mytype==1): # if mytype==gongbing
         if encType==1 or encType == 11:#(encType == gongbing || encType == zhadan)
             return 2
         elif encType==10: #encType == dilei:
             return 1
         else : 
             return 0
-    elif mytype == 10: #mytype == dilei
-        if encType == 1 :#encType == gongbing
+    elif mytype == 10: # mytype == dilei
+        if encType == 1 :# encType == gongbing
             return 0
-        elif encType==11:#encType == zhadan
+        elif encType==11:# encType == zhadan
             return 2
         else :
             return 1
-    #涉及炸弹的特判
+    # 涉及炸弹的特判
     if mytype==11 or encType==11:
         return 2
     else:
@@ -56,29 +63,30 @@ def ChessComparisons(myc,enc,node):#finish
             return 1
         else :
             return 2
+
 def codeToType(code):#finish
-    if code==1:#siling
-        return 9
-    if code==2:#junzhang
-        return 8
-    if code==3:#shizhang
-        return 7
-    if code==4:#lvzhang
-        return 6
-    if code==5:#tuanzhang
-        return 5
-    if code==6:#yingzhang
-        return 4
-    if code==7:#lianzhang
-        return 3
-    if code==8:#paizhang
-        return 2
-    if code==9:#gongbing
-        return 1
-    if code==10:#dilei
-        return 10
-    if code==11:#zhadan
-        return 11
+    if code==1:
+        return basic.siling
+    if code==2:
+        return basic.junzhang
+    if code==3:
+        return basic.shizhang
+    if code==4:
+        return basic.lvzhang
+    if code==5:
+        return basic.tuanzhang
+    if code==6:
+        return basic.yingzhang
+    if code==7:
+        return basic.lianzhang
+    if code==8:
+        return basic.paizhang
+    if code==9:
+        return basic.gongbing
+    if code==10:
+        return basic.dilei
+    if code==11:
+        return basic.zhadan
     if code==12 or code==14:
-        return 0
+        return basic.junqi
     return -1
--- simulate.py
+++ simulate.py
@@ -1,5 +1,5 @@
-import basic
 import asses
+
 def findChess(x,y,posList):
     sub = 0
     for xi,yi in posList:
@@ -7,54 +7,52 @@ def findChess(x,y,posList):
             return sub
         else:
             sub += 1
-def codeToStrength(type):
-    if type==11 : # zhadan=11
-        return 7 #炸弹=师长
-    elif type==10: #dilei=10
-        return 5 #地雷=团长
-    else :
-        return type
+
 def simMove(node,x1,y1,x2,y2,isEne):
-    newcMap=node.cMap[:] #新开一块棋盘做修改
-    newPos= newcMap[y2][x2]
-    oldPos= newcMap[y2][x2]
-    if(newPos==0):
+    cMap = node.cMap[:]
+    posList = node.posList[:]
+
+    if(cMap[y2][x2]==0):
         if(isEne):
             enemyChess=findChess(x1,y1,node.posList)
-            node.proTable[enemyChess]
-        newPos=oldPos
-        oldPos=0
+            node.posList[enemyChess] = (x2,y2)
+        cMap[y2][x2]=cMap[y1][x1]
+        cMap[y1][x1]=0
         Ismove=True
-        return newcMap,Ismove,node.posList
+        return cMap,Ismove,posList
     else:
         if isEne==False:
-            result=asses.ChessComparisons(oldPos,findChess(x2,y2,node.posList),node)
+            result=asses.ChessComparisons(cMap[y1][x1],findChess(x2,y2,node.posList),node)
         else:
-            result=asses.ChessComparisons(newPos,findChess(x1,y1,node.posList),node)
-        if result==0:#实际的地方胜
+            result=asses.ChessComparisons(cMap[y2][x2],findChess(x1,y1,node.posList),node)
+
+        if result==0: # 实际的敌方胜
             if isEne:
                 enemyChess=findChess(x1,y1,node.posList)
-                asses.setDie(enemyChess,node.proList)#e->setPos(x2, y2);
-                newPos=oldPos
-                oldPos=0
+                node.posList[enemyChess] = (x2, y2)
+                cMap[y2][x2]=cMap[y1][x1]
+                cMap[y1][x1]=0
             else:
-                oldPos=0
-        elif result==1:#实际的我方胜
+                cMap[y1][x1]=0
+
+        elif result==1: # 实际的我方胜
             if not isEne:
                 enemyChess=findChess(x2,y2,node.proList)
-                asses.setDie(enemyChess,node.posList)
-                newPos=oldPos
-                oldPos=0
-            else:#老位置是实际的敌方，前进失败
+                asses.setDie(enemyChess,posList)
+                cMap[y2][x2]=cMap[y1][x1]
+                cMap[y1][x1]=0
+            else: # 老位置是实际的敌方，前进失败
                 enemyChess=findChess(x1,y1,node.proList)
-                asses.setDie(enemyChess,node.proList)
-                oldPos=0
-        elif result==2:#对死
+                asses.setDie(enemyChess,posList)
+                cMap[y1][x1]=0
+
+        elif result==2: # 对死
             if isEne:
                 enemyChess=findChess(x1,y1,node.posList)
             else:
                 enemyChess=findChess(x2,y2,node.posList)
-            asses.setDie(enemyChess,node.posList)
-            newPos=0
-            oldPos=0
-        return newcMap,Ismove,node.posList
+            asses.setDie(enemyChess,posList)
+            cMap[y2][x2]=0
+            cMap[y1][x1]=0
+
+        return cMap,Ismove,posList
