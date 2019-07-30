21b3842e5465f82100ffa62283b666e86ae4ae84
3A <524019351@qq.com>
2019-07-30 16:05:35 +0800

    改错

--- playout.py
+++ playout.py
@@ -12,13 +12,18 @@ def tableToStr(table):
 def _playout(node):
     p = subprocess.Popen('playout.exe', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     # 先发送局面和得到的信息
-    p.stdin.writelines(str(node.isEne))
-    p.stdin.writelines(tableToStr(node.cMap))
-    p.stdin.writelines(tableToStr(node.probTable))
-    p.stdin.writelines(tableToStr(node.posList))
+    p.stdin.write(str(node.isEne).encode())
+    p.stdin.flush()
+    p.stdin.write(tableToStr(node.cMap).encode())
+    p.stdin.flush()
+    p.stdin.write(tableToStr(node.probTable).encode())
+    p.stdin.flush()
+    p.stdin.write(tableToStr(node.posList).encode())
+    p.stdin.flush()
     # 发送局面后接收结果即可
     while True:
         line = p.stdout.readline() # 返回的是GO 坐标坐标
+        line = line.decode()
         if line[0]=='G':
             oldPos = (int(line[3]),int(line[4]))
             newPos = (int(line[5],int(line[6])))
