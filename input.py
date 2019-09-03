0a75d71c667e1dc13a758da29a2af85129bbdd87
Iridium <784931837@qq.com>
2019-09-03 20:42:47 +0800

    拆分为子函数

--- input.py
+++ input.py
@@ -1,6 +1,42 @@
 import train
 import os
 
+def inputMap(inmap,start,end,isFirst):        # 读入棋盘
+    temmap = [[0 for i in range(5)] for i in range(12)]
+    for i in range(start,end):
+        if(isFirst==True):
+            temmap1 = inmap[i].split()
+            temmap2 = ['0' if i == '\x00' else i for i in temmap1]
+            temmap2 = [int(x) for x in temmap2]
+            temmap[i] = temmap2
+        else:
+            temmap1 = inmap[i].split()
+            temmap2 = ['0' if i == '\x00' else i for i in temmap1]
+            temmap2 = [int(x) for x in temmap2]
+            temmap[i-1] = temmap2
+    return temmap
+
+def inputProb(inprob,start,end):
+    temProb = [[0 for i in range(12)] for i in range(25)]
+    for i in range(start, end):
+        temProb1 = inprob[i].split()
+        temProb1 = [float(x) for x in temProb1]
+        temProb[i - 1] = temProb1
+    return temProb
+
+def inputPos(inpos,start,end):
+    temPos = [[0 for i in range(2)] for i in range(25)]
+    for i in range(start, end):
+        temPos1 = inpos[i].split()
+        temPos1 = [int(x) for x in temPos1]
+        temPos[i - 1] = temPos1
+    return temPos
+
+def inputRounds(temRounds):
+    temmRounds = temRounds.split()
+    temmRounds = [int(x) for x in temmRounds]
+    return temmRounds[0]
+
 for root, dirs, files in os.walk(r"D:/日记"):
     for file in files:
         f = open(os.path.join(root, file), "r")
@@ -14,41 +50,26 @@ for root, dirs, files in os.walk(r"D:/日记"):
         tempwinorlose = diary[len(diary) - 1].split()
         tempwinorlose = [int(x) for x in tempwinorlose]
         winorlose = bool(tempwinorlose[0])
-        isfirst = False
-        j = 0
+        isfirst = True
         for i in range(len(diary) - 1):
-            outdiary[i] = diary[i].split("$")  # 读入棋盘
-            tempmap1 = outdiary[i][0].split("\n")
+            outdiary[i] = diary[i].split("$")  # 读入日记
+            tempmap = outdiary[i][0].split("\n")
             tempProb1 = outdiary[i][1].split("\n")
             tempPos1 = outdiary[i][2].split("\n")
-            tempRounds = outdiary[i][4].split()
+            tempRounds1 = outdiary[i][4]
             tempchessNum = outdiary[i][3].split()
             tempassess = outdiary[i][5].split()
-            for i in range(12):
-                if ((isfirst == False) or (j < 12)):
-                    tempmap2 = tempmap1[i].split()
-                    tempmap3 = ['0' if i == '\x00' else i for i in tempmap2]
-                    tempmap3 = [int(x) for x in tempmap3]
-                    chessMap[i] = tempmap3
-                    j = j + 1
-                    isfirst = True
-                else:
-                    tempmap2 = tempmap1[i + 1].split()
-                    tempmap3 = ['0' if i == '\x00' else i for i in tempmap2]
-                    tempmap3 = [int(x) for x in tempmap3]
-                    chessMap[i] = tempmap3
-            for i in range(1, 26):
-                tempProb2 = tempProb1[i].split()
-                tempProb2 = [float(x) for x in tempProb2]
-                chessProb[i - 1] = tempProb2  # 输出问题已解决
-            for i in range(1, 26):
-                tempPos2 = tempPos1[i].split()
-                tempPos2 = [int(x) for x in tempPos2]
-                chessPos[i - 1] = tempPos2
-            tempRounds = [int(x) for x in tempRounds]
+            if(isfirst==True):
+                chessMap=inputMap(tempmap,0,12,isfirst)
+                isfirst = False
+            else:
+                chessMap=inputMap(tempmap,1,13,isfirst)
+            chessProb=inputProb(tempProb1,1,26)
+            chessPos=inputPos(tempPos1,1,26)
+            tempRounds = inputRounds(tempRounds1)
             tempchessNum = [int(x) for x in tempchessNum]
             tempassess = [float(x) for x in tempassess]
-            chessOther[0] = tempRounds[0]
+            chessOther[0] = tempRounds
             for i in range(2):
                 chessOther[i + 1] = tempchessNum[i]
             for i in range(7):
